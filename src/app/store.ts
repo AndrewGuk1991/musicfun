dbc3b0162ab6b14910f6ddc41b6b05c267723ad6
andrew_guk <gukandrey91@gmail.com>
2026-08-10 19:03:03 +0300

    connect baseApi to a store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import {playlistsApi} from "@/features/playlists/api/playlists/playlistsApi.ts";
+import {configureStore} from '@reduxjs/toolkit'
+import {setupListeners} from '@reduxjs/toolkit/query'
+import {baseApi} from "@/app/baseApi.ts";
 
 export const store = configureStore({
     reducer: {
-        [playlistsApi.reducerPath]: playlistsApi.reducer,
+        [baseApi.reducerPath]: baseApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(playlistsApi.middleware),
+        getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 
