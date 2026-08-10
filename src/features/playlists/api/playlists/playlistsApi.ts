636ad0ba9250bd9f5684ca7d0ee350d3b4b26dba
andrew_guk <gukandrey91@gmail.com>
2026-08-10 18:51:40 +0300

    add a cache tag system to automate re-fetching

--- src/features/playlists/api/playlists/playlistsApi.ts
+++ src/features/playlists/api/playlists/playlistsApi.ts
@@ -18,29 +18,34 @@ export const playlistsApi = createApi({
             return headers
         },
     }),
+    tagTypes: ['Playlist'],
     endpoints: (build) => ({
         fetchPlaylists: build.query<PlaylistsResponse, void>({
-            query: () => `playlists`
+            query: () => `playlists`,
+            providesTags: ['Playlist']
         }),
         createPlaylist: build.mutation<{data: PlaylistData}, CreatePlaylistArgs>({
             query: body => ({
                 url: 'playlists',
                 method: 'post',
                 body
-            })
+            }),
+            invalidatesTags: ['Playlist']
         }),
         deletePlaylist: build.mutation<void, string>({
             query: (playlistId) => ({
                 url: `playlists/${playlistId}`,
                 method: 'delete',
-            })
+            }),
+            invalidatesTags: ['Playlist']
         }),
         updatePlaylist: build.mutation<void, {playlistId: string, body: UpdatePlaylistArgs}>({
             query: ({playlistId, body}) => ({
                 url: `playlists/${playlistId}`,
                 method: 'put',
                 body
-            })
+            }),
+            invalidatesTags: ['Playlist']
         })
     })
 })
