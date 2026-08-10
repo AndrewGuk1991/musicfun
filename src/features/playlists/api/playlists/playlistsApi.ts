d6049718c07ecbb9e5bb7576dbad1760d3e2cb5c
andrew_guk <gukandrey91@gmail.com>
2026-08-10 18:59:29 +0300

    refactoring playlistsApi - implementation of baseApi

--- src/features/playlists/api/playlists/playlistsApi.ts
+++ src/features/playlists/api/playlists/playlistsApi.ts
@@ -1,24 +1,13 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import type {
     CreatePlaylistArgs,
     PlaylistData,
-    PlaylistsResponse, UpdatePlaylistArgs
+    PlaylistsResponse,
+    UpdatePlaylistArgs
 } from "@/features/playlists/api/playlists/playlistsApi.types.ts";
+import {baseApi} from "@/app/baseApi.ts";
 
 
-export const playlistsApi = createApi({
-    reducerPath: 'playlistsApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL,
-        headers: {
-            'API-KEY': import.meta.env.VITE_API_KEY,
-        },
-        prepareHeaders: headers => {
-            headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-            return headers
-        },
-    }),
-    tagTypes: ['Playlist'],
+export const playlistsApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         fetchPlaylists: build.query<PlaylistsResponse, void>({
             query: () => `playlists`,
