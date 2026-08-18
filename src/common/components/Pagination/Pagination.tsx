9bdd700246370775a3f2d8d989e3264aab5d8b21
andrew_guk <gukandrey91@gmail.com>
2026-08-18 19:58:28 +0300

    add Pagination and styles

---
+++ src/common/components/Pagination/Pagination.tsx
@@ -0,0 +1,43 @@
+
+import s from './Pagination.module.css'
+import {getPaginationPages} from "@/common/utils";
+
+type Props = {
+    currentPage: number
+    setCurrentPage: (page: number) => void
+    pagesCount: number
+}
+
+export const Pagination = ({pagesCount, setCurrentPage, currentPage}: Props) => {
+    if (pagesCount <= 1) return null
+
+    const pages = getPaginationPages(currentPage, pagesCount)
+
+    return (
+        <div className={s.pagination}>
+            {
+                pages.map((page, idx) =>
+                    page === '...' ? (
+                        <span className={s.ellipsis} key={`ellipsis-${idx}`}>
+                            ...
+                        </span>
+                    ) : (
+                        <button
+                            key={page}
+                            className={
+                                page === currentPage ? `${s.pageButton} ${s.pageButtonActive}` : s.pageButton
+                            }
+                            onClick={() => page !== currentPage && setCurrentPage(Number(page))}
+                            disabled={page === currentPage}
+                            type={'button'}
+                        >
+                            {page}
+                        </button>
+                    )
+
+                )
+            }
+        </div>
+
+    )
+}
