86d7072382ba2eea7d4ac8814c1c48e83d9f6615
andrew_guk <gukandrey91@gmail.com>
2026-08-22 19:20:22 +0300

    use useFetchTracksQuery in TracksPage

--- src/features/tracks/ui/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage.tsx
@@ -1,6 +1,11 @@
+import {useFetchTracksQuery} from "@/features/tracks/api/tracksApi.ts";
 
 
 export const  TracksPage = () => {
+
+    const {data} = useFetchTracksQuery()
+    console.log(data)
+
     return (
         <>
             <h2>TracksPage</h2>
