ace6d08665b6914ce37cd50af5ceec310751e7da
andrew_guk <gukandrey91@gmail.com>
2026-08-23 17:03:38 +0300

    refactoring tracksApi - replace build.query to build.infiniteQuery and add useFetchTracksInfiniteQuery

--- src/features/tracks/api/tracksApi.ts
+++ src/features/tracks/api/tracksApi.ts
@@ -4,12 +4,21 @@ import type {FetchTracksResponse} from "@/features/tracks/api/tracksApi.types.ts
 
 export const tracksApi = baseApi.injectEndpoints({
     endpoints: build => ({
-        fetchTracks: build.query<FetchTracksResponse, void>({
-            query: () => ({
-                url: 'playlists/tracks'
-            })
+        fetchTracks: build.infiniteQuery<FetchTracksResponse, void, string | null>({
+            infiniteQueryOptions: {
+                initialPageParam: null,
+                getNextPageParam: (lastPage) => (
+                    lastPage.meta.nextCursor || null
+                )
+            },
+            query: ({pageParam}) => (
+                {
+                    url: 'playlists/tracks',
+                    params: {cursor: pageParam, paginationType: 'cursor', pageSize: 5}
+                }
+            )
         })
     })
 })
 
-export const {useFetchTracksQuery} = tracksApi
+export const {useFetchTracksInfiniteQuery} = tracksApi
