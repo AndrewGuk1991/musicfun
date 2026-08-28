2e85cb9a72d9257e1b3f7323c0d612612c394998
andrew_guk <gukandrey91@gmail.com>
2026-08-28 18:09:27 +0300

    add isErrorWithError

---
+++ src/common/utils/isErrorWithError.ts
@@ -0,0 +1,10 @@
+
+
+export const isErrorWithError = (error: unknown): error is {error: string} => {
+    return (
+        typeof error === 'object' &&
+            error !== null &&
+            'error' in error &&
+            typeof (error as any).error === 'string'
+    )
+}
