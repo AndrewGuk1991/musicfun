9f3408322c04037cbe40bb6e354ded9ebe9e9532
andrew_guk <gukandrey91@gmail.com>
2026-09-01 18:34:48 +0300

    use trimToMaxLength in baseApi and add case 400 to error handling

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {toast} from "react-toastify";
-import {isErrorWithDetailArray, isErrorWithProperty} from "@/common/utils";
+import {isErrorWithDetailArray, isErrorWithProperty, trimToMaxLength} from "@/common/utils";
 
 
 export const baseApi = createApi({
@@ -44,9 +44,10 @@ export const baseApi = createApi({
                         toast(JSON.stringify(result.error.data), { type: 'error', theme: 'colored' })
                     }
                     break
+                case 400:
                 case 403:
                     if (isErrorWithDetailArray(result.error.data)) {
-                        toast(result.error.data.errors[0].detail, { type: 'error', theme: 'colored' })
+                        toast(trimToMaxLength(result.error.data.errors[0].detail), { type: 'error', theme: 'colored' })
                     } else {
                         toast(JSON.stringify(result.error.data), { type: 'error', theme: 'colored' })
                     }
