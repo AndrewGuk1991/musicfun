f68c4ff1a3f7eb805c2f1437a65d37e067fc18e0
andrew_guk <gukandrey91@gmail.com>
2026-09-04 18:19:47 +0300

    refactoring baseApi - use accessToken from localStorage

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,13 +1,12 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {handleErrors} from "@/common/utils";
+import {AUTH_KEYS} from "@/common/constants";
 
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    tagTypes: ['Playlist'],
+    tagTypes: ['Playlist', 'Auth'],
     baseQuery: async (args, api, extraOptions) => {
-
-        // await new Promise(resolve => setTimeout(resolve, 2000))
 
         const result = await fetchBaseQuery({
             baseUrl: import.meta.env.VITE_BASE_URL,
@@ -15,7 +14,10 @@ export const baseApi = createApi({
                 'API-KEY': import.meta.env.VITE_API_KEY,
             },
             prepareHeaders: headers => {
-                headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+                const accessToken = localStorage.getItem(AUTH_KEYS.accessToken)
+                if (accessToken) {
+                    headers.set('Authorization', `Bearer ${accessToken}`)
+                }
                 return headers
             },
         })(args, api, extraOptions)
