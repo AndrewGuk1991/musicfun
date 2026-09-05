f9f4d02704eb2d362bd7b6b0d3510a7dfd39f981
andrew_guk <gukandrey91@gmail.com>
2026-09-05 14:59:14 +0300

    add isTokens in utils

---
+++ src/common/utils/isTokens.ts
@@ -0,0 +1,9 @@
+
+export const isTokens = (data: unknown): data is { accessToken: string; refreshToken: string } => {
+    return (
+        typeof data === 'object'
+        && data !== null
+        && 'accessToken' in data
+        && 'refreshToken' in data
+    )
+}
