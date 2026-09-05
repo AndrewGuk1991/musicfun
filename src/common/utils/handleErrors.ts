50465891da4520c4f13e2aee1fb6a84dbb67f421
andrew_guk <gukandrey91@gmail.com>
2026-09-05 14:18:46 +0300

    delete case 401 from handleErrors

--- src/common/utils/handleErrors.ts
+++ src/common/utils/handleErrors.ts
@@ -33,7 +33,6 @@ export const handleErrors = (error: FetchBaseQueryError) => {
                 }
                 break
 
-            case 401:
             case 429:
                 if (isErrorWithProperty(error.data, 'message')) {
                    errorToast(error.data.message)
