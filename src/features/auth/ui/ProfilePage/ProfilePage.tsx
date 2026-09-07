708a707af2a30458034da57e5088829a4388e1a7
andrew_guk <gukandrey91@gmail.com>
2026-09-07 23:52:44 +0300

    refactoring ProfilePage - add useFetchPlaylistsQuery by userId and painting playlists

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,31 @@
 import {useGetMeQuery} from "@/features/auth/api/authApi.ts";
-
+import {useFetchPlaylistsQuery} from "@/features/playlists/api/playlists/playlistsApi.ts";
+import {CreatePlaylistForm, PlaylistsList} from "@/features/playlists/ui";
+import s from './ProfilePage.module.css'
+import {Navigate} from "react-router";
+import {Path} from "@/common/routing";
 
 export const ProfilePage = () => {
 
-   const {data} = useGetMeQuery()
+    const {data: meResponse, isLoading: isMeLoading} = useGetMeQuery()
+
+
+    const {data: playlistsResponse, isLoading} = useFetchPlaylistsQuery(
+        {userId: meResponse?.userId},
+        {skip: !meResponse?.userId}
+    )
+
+    if(isMeLoading || isLoading) return <h1>Skeleton loader ...</h1>
+
+    if(!isMeLoading && !meResponse) return <Navigate to={Path.Playlists}/>
 
     return (
-        <h1>{data?.login} page</h1>
+        <div>
+            <h1>{meResponse?.login} page</h1>
+            <div className={s.container}>
+                <CreatePlaylistForm/>
+                <PlaylistsList isPlaylistLoading={isLoading || isMeLoading} playlists={playlistsResponse?.data || []}/>
+            </div>
+        </div>
     )
 }
