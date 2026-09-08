b733ffe7d1694a7ee264ae19d8b5b2f5719bc448
andrew_guk <gukandrey91@gmail.com>
2026-09-08 19:29:40 +0300

    refactoring common types.ts - export types with zod infer

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,23 +1,17 @@
+import {
+    type coverSchema,
+    currentUserReactionSchema,
+    type imagesSchema,
+    type tagSchema,
+    type userSchema
+} from "@/common/schemas"
 
-export type Tag = {
-    id: string
-    name: string
-}
+import * as z from "zod"
 
-export type User = {
-    id: string
-    name: string
-}
 
-export type Images = {
-    main: Cover[]
-}
 
-export type Cover = {
-    type: 'original' | 'medium' | 'thumbnail'
-    width: number
-    height: number
-    fileSize: number
-    url: string
-}
-
+export type Tag = z.infer<typeof tagSchema>
+export type User = z.infer<typeof userSchema>
+export type Cover = z.infer<typeof coverSchema>
+export type Images = z.infer<typeof imagesSchema>
+export type CurrentUserReaction = z.infer<typeof currentUserReactionSchema>
