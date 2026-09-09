aeaf9677945b421d420c1a0ba7507235150c8bbf
andrew_guk <gukandrey91@gmail.com>
2026-09-09 20:32:22 +0300

    refactoring CreatePlaylistForm - use zodResolver and output errors messages

--- src/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx
+++ src/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx
@@ -1,9 +1,28 @@
 import {type SubmitHandler, useForm} from "react-hook-form";
 import type {CreatePlaylistArgs} from "@/features/playlists/api/playlists/playlistsApi.types.ts";
 import {useCreatePlaylistMutation} from "@/features/playlists/api/playlists/playlistsApi.ts";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {createPlaylistSchema} from "@/features/playlists/model/playlists.schemas.ts";
+import s from './CreatePlaylistForm.module.css'
 
 export const CreatePlaylistForm = () => {
-    const {register, handleSubmit, reset} = useForm<CreatePlaylistArgs>({defaultValues: {data: {type: 'playlists'}}})
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors},
+    } = useForm<CreatePlaylistArgs>({
+        resolver: zodResolver(createPlaylistSchema),
+        defaultValues: {
+            data: {
+                type: 'playlists',
+                attributes: {
+                    title: '',
+                    description: ''
+                }
+            }
+        }
+    })
 
     const [createPlaylist] = useCreatePlaylistMutation()
 
@@ -18,9 +37,13 @@ export const CreatePlaylistForm = () => {
             <h2>Create new playlist</h2>
             <div>
                 <input {...register('data.attributes.title')} placeholder={'title'}/>
+                {errors.data?.attributes?.title &&
+                    <span className={s.error}>{errors.data.attributes.title.message}</span>}
             </div>
             <div>
                 <input {...register('data.attributes.description')} placeholder={'description'}/>
+                {errors.data?.attributes?.description &&
+                    <span className={s.error}>{errors.data.attributes.description.message}</span>}
             </div>
             <button>create playlist</button>
         </form>
