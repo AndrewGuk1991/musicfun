0075c09730aba2e58920967b1b3bdb6dcf5aecb1
andrew_guk <gukandrey91@gmail.com>
2026-09-09 23:26:06 +0300

    refactoring api types - use api schemas

--- src/features/auth/api/authApi.types.ts
+++ src/features/auth/api/authApi.types.ts
@@ -1,14 +1,9 @@
+import {loginResponseSchema, type meResponseSchema} from "@/features/auth/model/api.schemas.ts";
+import * as z from "zod";
 
+export type MeResponse = z.infer<typeof meResponseSchema>;
 
-export type MeResponse = {
-    userId: string
-    login: string
-}
-
-export type LoginResponse = {
-    refreshToken: string
-    accessToken: string
-}
+export type LoginResponse = z.infer<typeof loginResponseSchema>
 
 export type LoginArgs = {
     code: string
@@ -16,3 +11,4 @@ export type LoginArgs = {
     accessTokenTTL?: string
     rememberMe: boolean
 }
+
