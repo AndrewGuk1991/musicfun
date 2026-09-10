2495ca913d5e2045ca23db3488178bdf60c89583
andrew_guk <gukandrey91@gmail.com>
2026-09-10 21:11:04 +0300

    add skipSchemaValidation during production

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,10 +1,12 @@
 import {createApi} from "@reduxjs/toolkit/query/react";
 import {baseQueryWithReauth} from "@/app/api/baseQueryWithReauth.ts";
 
+
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     tagTypes: ['Playlist', 'Auth'],
     endpoints: () => ({}),
     baseQuery: baseQueryWithReauth,
+    skipSchemaValidation: process.env.NODE_ENV === 'production',
 })
 
