02288dee3e43fbea6ad31dc03ebf888e0b999d6f
andrew_guk <gukandrey91@gmail.com>
2026-09-13 19:35:23 +0300

    add vite-plugin-svg-icons and fast-glob

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+    plugins: [
+        react(),
+        createSvgIconsPlugin({
+
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+
+            symbolId: 'icon-[name]',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@/': `${path.resolve(__dirname, 'src')}/`,
+        },
     },
-  },
 })
