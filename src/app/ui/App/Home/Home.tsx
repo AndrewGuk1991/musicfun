9e751f6e0e761841aa9e8b9a8ef53b10523f2135
andrew_guk <gukandrey91@gmail.com>
2026-09-15 21:26:10 +0300

    refactoring Home - add 10 last playlists and tracks

--- src/app/ui/App/Home/Home.tsx
+++ src/app/ui/App/Home/Home.tsx
@@ -1,13 +1,30 @@
-import {useGetMeQuery} from "@/features/auth/api/authApi.ts";
+import {useFetchPlaylistsQuery} from "@/features/playlists/api/playlists/playlistsApi.ts";
+import {PlaylistsList} from "@/features/playlists/ui";
+import {useFetchLastTracksQuery} from "@/features/tracks/api/tracksApi.ts";
+import {TracksList} from "@/features/tracks/ui/TracksList/TracksList.tsx";
+import s from './Home.module.css'
 
 export const Home = () => {
 
-    const {data} = useGetMeQuery()
+    const {data, isLoading} = useFetchPlaylistsQuery({pageSize: 10})
+
+    const { data: tracksData } = useFetchLastTracksQuery({ pageSize: 10 });
+
+
+    const tracks = tracksData?.data || []
 
     return (
-        <div>
-            <h1>Home </h1>
-            <div>login: {data?.login} </div>
-        </div>
+        <section className={s.home}>
+            <div>
+                <h2>New playlists</h2>
+                <PlaylistsList playlists={data?.data || []} isPlaylistLoading={isLoading} />
+            </div>
+            <div>
+                <h2>New tracks</h2>
+                <TracksList tracks={tracks}/>
+            </div>
+
+
+        </section>
     )
 }
