93ff08c6114b594e18dcaab69fd36e86bef7e413
andrew_guk <gukandrey91@gmail.com>
2026-09-17 22:11:54 +0300

    refactoring PlaylistsList - add modal with EditPlaylistForm

--- src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx
@@ -5,23 +5,38 @@ import {useState} from "react";
 import {useDeletePlaylistMutation} from "@/features/playlists/api/playlists/playlistsApi.ts";
 import type {PlaylistData, UpdatePlaylistArgs} from "@/features/playlists/api/playlists/playlistsApi.types.ts";
 import {useForm} from "react-hook-form";
+import defaultCover from "@/assets/images/default-playlist-cover.png";
+import {Modal} from "@/common/components/Modal/Modal.tsx";
 
 type Props = {
     playlists: PlaylistData[]
     isPlaylistLoading: boolean
 }
 
+export type EditFormValues = UpdatePlaylistArgs & {
+    data: {
+        attributes: {
+            coverUrl?: string;
+            coverFile?: FileList;
+        }
+    }
+}
+
 export const PlaylistsList = ({playlists, isPlaylistLoading}: Props) => {
-    const [editingPlaylistId, setEditingPlaylistId] = useState<string | null>(null)
+    const [activePlaylist, setActivePlaylist] = useState<PlaylistData | null>(null)
 
 
-    const {register, handleSubmit, reset} = useForm<UpdatePlaylistArgs>(
+    const {register, handleSubmit, reset, watch} = useForm<EditFormValues>(
         {
             defaultValues: {
                 data: {
                     type: 'playlists',
                     attributes: {
-                        tagIds: ['']
+                        title: '',
+                        description: '',
+                        tagIds: [''],
+                        coverUrl: '',
+                        coverFile: undefined
                     }
                 },
             }
@@ -37,9 +52,11 @@ export const PlaylistsList = ({playlists, isPlaylistLoading}: Props) => {
         }
     }
 
+
     const editPlaylistHandler = (playlist: PlaylistData | null) => {
+        setActivePlaylist(playlist)
+
         if (playlist) {
-            setEditingPlaylistId(playlist.id)
             reset({
                 data: {
                     type: 'playlists',
@@ -47,42 +64,46 @@ export const PlaylistsList = ({playlists, isPlaylistLoading}: Props) => {
                         title: playlist.attributes.title,
                         description: 'описание не приходит с бэка',
                         tagIds: playlist.attributes.tags.map(t => t.id),
+                        coverUrl: playlist.attributes.images.main?.find(img => img.type === 'original')?.url || defaultCover
                     }
                 }
             })
         } else {
-            setEditingPlaylistId(null)
+            reset()
         }
+    }
+
+    const handleCloseModal = () => {
+        editPlaylistHandler(null)
     }
 
     return (
         <div className={s.items}>
             {!playlists.length && !isPlaylistLoading && <h2>Playlists not found</h2>}
             {playlists.map((playlist) => {
-
-                const isEditingPlaylist = editingPlaylistId === playlist.id
-
                 return <div className={s.item} key={playlist.id}>
-
-                    {
-                        isEditingPlaylist ?
-                            <EditPlaylistForm
-                                editingPlaylistId={editingPlaylistId}
-                                setEditingPlaylistId={setEditingPlaylistId}
-                                editPlaylist={editPlaylistHandler}
-                                handleSubmit={handleSubmit}
-                                register={register}
-                            />
-                            :
-                            <PlaylistItem
-                                playlist={playlist}
-                                deletePlaylistHandler={deletePlaylistHandler}
-                                editPlaylistHandler={editPlaylistHandler}
-                            />
-
-                    }
+                    <PlaylistItem
+                        playlist={playlist}
+                        deletePlaylistHandler={deletePlaylistHandler}
+                        editPlaylistHandler={editPlaylistHandler}
+                    />
                 </div>
-            })}
+            }
+            )}
+
+            {activePlaylist && (
+                <Modal isOpen={Boolean(activePlaylist)} onClose={handleCloseModal}>
+                    <h3>Редактировать плейлист</h3>
+
+                    <EditPlaylistForm
+                        register={register}
+                        handleSubmit={handleSubmit}
+                        watch={watch}
+                        editingPlaylistId={activePlaylist.id}
+                        onClose={handleCloseModal}
+                    />
+                </Modal>
+            )}
         </div>
     )
 }
