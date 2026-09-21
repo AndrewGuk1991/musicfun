42647a25a826db6441a9fb1209684d5519dd9155
andrew_guk <gukandrey91@gmail.com>
2026-09-21 23:57:28 +0300

    add reactionPlaylistResponseSchema

--- src/features/playlists/model/playlists.schemas.ts
+++ src/features/playlists/model/playlists.schemas.ts
@@ -53,3 +53,10 @@ export const playlistsResponseSchema = z.object({
 export const playlistCreateResponseSchema = z.object({
     data: playlistDataSchema,
 })
+
+export const reactionPlaylistResponseSchema = z.object({
+    objectId: z.string(),
+    value: currentUserReactionSchema,
+    likes: z.int().nonnegative(),
+    dislikes: z.int().nonnegative(),
+})
