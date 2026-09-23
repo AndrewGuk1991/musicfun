a3e158cae3464fe5a75c31024dc3f107015b964d
andrew_guk <gukandrey91@gmail.com>
2026-09-23 19:51:31 +0300

    refactoring PlaylistsList - add useCallback, useMemo

--- src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx
@@ -1,9 +1,8 @@
 import s from './PlaylistsList.module.css'
 import modalStyles from '@/common/components/Modal/Modal.module.css'
-
 import {EditPlaylistForm} from "@/features/playlists/ui/PlaylistsPage/EditPlaylistForm/EditPlaylistForm.tsx";
 import {PlaylistItem} from "@/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx";
-import {useState} from "react";
+import {useCallback, useMemo, useState} from "react";
 import {useDeletePlaylistMutation} from "@/features/playlists/api/playlists/playlistsApi.ts";
 import type {PlaylistData} from "@/features/playlists/api/playlists/playlistsApi.types.ts";
 import {Modal} from "@/common/components/Modal/Modal.tsx";
@@ -19,36 +18,39 @@ export const PlaylistsList = ({playlists, isPlaylistLoading}: Props) => {
 
     const [deletePlaylist] = useDeletePlaylistMutation()
 
-    const uniquePlaylists = playlists.filter(
-        (playlist, index, self) => self.findIndex((p) => p.id === playlist.id) === index
-    )
+    const uniquePlaylists = useMemo(() => {
+        return playlists.filter(
+            (playlist, index, self) => self.findIndex((p) => p.id === playlist.id) === index
+        )
+    }, [playlists])
 
-    const deletePlaylistHandler = (playlistId: string) => {
+    const deletePlaylistHandler = useCallback((playlistId: string) => {
         if (confirm('Are you sure you want to delete this playlist?')) {
             deletePlaylist(playlistId)
         }
-    }
+    }, [deletePlaylist])
 
-    const editPlaylistHandler = (playlist: PlaylistData | null) => {
+    const editPlaylistHandler = useCallback((playlist: PlaylistData | null) => {
         setActivePlaylist(playlist)
-    }
+    }, [])
 
-    const handleCloseModal = () => {
+    const handleCloseModal = useCallback(() => {
         setActivePlaylist(null)
-    }
+    }, [])
 
     return (
         <div className={s.items}>
             {!uniquePlaylists.length && !isPlaylistLoading && <h2>Playlists not found</h2>}
-            {uniquePlaylists.map((playlist) => (
-                <div className={s.item} key={playlist.id}>
-                    <PlaylistItem
-                        playlist={playlist}
-                        deletePlaylistHandler={deletePlaylistHandler}
-                        editPlaylistHandler={editPlaylistHandler}
-                    />
-                </div>
-            ))}
+            {uniquePlaylists.map(
+                (playlist) => (
+                    <div className={s.item} key={playlist.id}>
+                        <PlaylistItem
+                            playlist={playlist}
+                            deletePlaylistHandler={deletePlaylistHandler}
+                            editPlaylistHandler={editPlaylistHandler}
+                        />
+                    </div>
+                ))}
 
             {activePlaylist && (
                 <Modal isOpen={Boolean(activePlaylist)} onClose={handleCloseModal}>
@@ -65,12 +67,14 @@ export const PlaylistsList = ({playlists, isPlaylistLoading}: Props) => {
                     </Modal.Body>
 
                     <Modal.Footer>
-                            <button type="button" className={modalStyles.btnCancel} onClick={handleCloseModal} disabled={isFormSubmitting}>
-                                Cancel
-                            </button>
-                            <button type="submit" form="edit-playlist-form" className={modalStyles.btnSave} disabled={isFormSubmitting}>
-                                {isFormSubmitting ? 'Saving...' : 'Save Changes'}
-                            </button>
+                        <button type="button" className={modalStyles.btnCancel} onClick={handleCloseModal}
+                                disabled={isFormSubmitting}>
+                            Cancel
+                        </button>
+                        <button type="submit" form="edit-playlist-form" className={modalStyles.btnSave}
+                                disabled={isFormSubmitting}>
+                            {isFormSubmitting ? 'Saving...' : 'Save Changes'}
+                        </button>
                     </Modal.Footer>
                 </Modal>
             )}
