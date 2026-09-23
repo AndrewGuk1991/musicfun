0b42575487c4667f9408a84c127f99e5e3109c22
andrew_guk <gukandrey91@gmail.com>
2026-09-23 19:54:28 +0300

    fix CurrentUserReaction to CurrentUserReactionValue

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -14,4 +14,4 @@ export type Tag = z.infer<typeof tagSchema>
 export type User = z.infer<typeof userSchema>
 export type Cover = z.infer<typeof coverSchema>
 export type Images = z.infer<typeof imagesSchema>
-export type CurrentUserReaction = z.infer<typeof currentUserReactionSchema>
+export type CurrentUserReactionValue = z.infer<typeof currentUserReactionSchema>;
