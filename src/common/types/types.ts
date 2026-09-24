da2680e25ee3c73e2b065dab42c0724f49002742
andrew_guk <gukandrey91@gmail.com>
2026-09-24 21:36:08 +0300

    add type ReactionUserResponse

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,17 +1,16 @@
 import {
     type coverSchema,
     currentUserReactionSchema,
-    type imagesSchema,
+    type imagesSchema, reactionUserResponseSchema,
     type tagSchema,
     type userSchema
 } from "@/common/schemas"
 
 import * as z from "zod"
 
-
-
 export type Tag = z.infer<typeof tagSchema>
 export type User = z.infer<typeof userSchema>
 export type Cover = z.infer<typeof coverSchema>
 export type Images = z.infer<typeof imagesSchema>
 export type CurrentUserReactionValue = z.infer<typeof currentUserReactionSchema>;
+export type ReactionUserResponse = z.infer<typeof reactionUserResponseSchema>
